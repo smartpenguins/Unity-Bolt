e2fe86dc2d62e5ab43f99debd52cbea7be1352df
smartpenguins <70032642+smartpenguins@users.noreply.github.com>
2020-11-30 23:44:13 -0800

    Added PunRPC Support

--- Photon/PhotonToBolt.cs
+++ Photon/PhotonToBolt.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using Bolt;
+using System.Linq;
 
 public class PhotonToBolt : MonoBehaviourPunCallbacks
 {
@@ -14,5 +15,17 @@ public class PhotonToBolt : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         CustomEvent.Trigger(gameObject, "OnJoinedRoom");
+    }
+
+    [PunRPC]
+    void RPCCall(string eventName)
+    {
+        CustomEvent.Trigger(gameObject, eventName);
+    }
+
+    [PunRPC]
+    void RPCCall(params object[] args)
+    {
+        CustomEvent.Trigger(gameObject, (string) args[0], args.Skip(1).ToArray());
     }
 }
